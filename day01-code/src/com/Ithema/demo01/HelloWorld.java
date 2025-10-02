462f2787cdbc1724fdf99a38c428c7637c54ddbc
sunset-echoes <2697144155@qq.com`>
2025-10-02 19:51:12 +0800

    添加 day01-code 目录(存放代码文件)

---
+++ day01-code/src/com/Ithema/demo01/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.Ithema.demo01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "枫花恋";
+        String age = "18岁";
+        String sex = "女";
+        String i = "C D D";
+        System.out.println("姓名：" + name);
+        System.out.println("三围" + i );
+        System.out.println("年龄：" + age);
+        System.out.println("性别：" + sex);
+
+
+    }
+}
